07dadbe2a677471f9992f29e46c58f79f233f4af
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-18 11:42:20 -0800

    Passes test to add country code props to currency obj

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,6 +1,8 @@
 export default class Currency {
-  constructor(baseCurrency, exchangeCurrency) {
+  constructor(baseCurrency, exchangeCurrency, baseCurrencyCode, exchangeCurrencyCode) {
     this.baseCurrency = baseCurrency;
-    this.exchangeCurrency = exchangeCurrency
+    this.exchangeCurrency = exchangeCurrency;
+    this.baseCurrencyCode = baseCurrencyCode;
+    this.exchangeCurrencyCode = exchangeCurrencyCode;
   } 
 }
