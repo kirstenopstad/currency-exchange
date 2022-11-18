df7fe6b6ad70697d9f06402f0549ffe016acdf33
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-18 09:46:44 -0800

    Add API call to get country codes

--- src/js/currency-exchange-service.js
+++ src/js/currency-exchange-service.js
@@ -1,1 +1,22 @@
-// Business Logic
+// Export API Call Service
+export default class CurrencyExchangeService {
+  // Create static method to gather country code data
+  static getCountryCodes() {
+    // Return promise
+    return fetch(`https://v6.exchangerate-api.com/v6/API_KEY/codes`)
+      // Validate response
+      .then(function(response) {
+        if (!response) {
+          throw new Error("API ERROR");
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+  // Create static method to gather country code data
+  static getExchangeRates() {
+  }
+}
