ce569c1faf296b863c614337d2485117f028f56d
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-18 09:32:37 -0800

    Add UI Logic to handle user input error

--- src/index.js
+++ src/index.js
@@ -13,12 +13,26 @@ function getExchangeValue() {
 }
 
 // UI Logic
-function handleFormSubmission {
-  // Get values of base amt & exchange currency
-    // Validate base amount is number
-    // Run getCurrencyCode(), store in variable
-  // Run getExchangeValue(baseAmt, currencyCode)
-  // Display value in exchange currency in DOM
+function handleFormSubmission() {
+  event.preventDefault();
+  // Return error if user inputs nothing 
+  if (!document.getElementById("input-base-currency").value || !document.getElementById("input-exchange-currency").value) {
+    document.querySelector("p#response").innerText = "Must enter base amount and currency to get exchange rate!"
+    return console.error("No input")
+  } else {
+    // Get values of base amt & exchange currency
+    const inputBaseAmt = document.getElementById("input-base-currency").value;
+    const inputCurrencyName = document.getElementById("input-exchange-currency").value;
+      // Validate base amount is number
+      // Run getCurrencyCode(), store in variable
+      // const currencyCode = getCurrencyCode(inputCurrencyName)
+      const currencyCode = "KRW"
+      console.log(getCurrencyCode(inputCurrencyName));
+    // Run getExchangeValue(baseAmt, currencyCode)
+    console.log(getExchangeValue(inputBaseAmt, currencyCode));
+    // Display value in exchange currency in DOM
+    document.querySelector("p#response").innerText = "hello!"
+  }
 }
 window.addEventListener("load", function() {
   // TODO: Get country codes & store in class 
