f4e3627c62e1310c7eb2b778299b355debe85461
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-18 11:40:56 -0800

    Add test to add currency codes props to Currency obj

--- __tests__/currency.test.js
+++ __tests__/currency.test.js
@@ -1,4 +1,4 @@
-import '../src/js/currency.js';
+import Currency from "../src/js/currency";'../src/js/currency.js';
 
 describe('Currency', () => {
 
@@ -7,4 +7,11 @@ describe('Currency', () => {
     expect(currency.baseCurrency).toEqual(0);
     expect(currency.exchangeCurrency).toEqual(0);
   });
+
+  test('It should return a currency object with baseCurrencyCode & exchangeCurrencyCode', () => {
+    let currency = new Currency(0,0,'USD','');
+    expect(currency.baseCurrencyCode).toEqual('USD');
+    expect(currency.exchangeCurrencyCode).toEqual('');
+  });
+
 })
