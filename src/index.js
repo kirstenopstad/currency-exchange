b7b288cdfb9a7b9c64a38d4444f310b996f41515
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-11-18 13:51:33 -0800

    WIP: Successfully stores country code array in currency object

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 // Business Logic
-function getAPIData(amt, currencyName) {
-  console.log(currencyName)
-  let currency = new Currency(amt, 'USD');
+function getAPIData(amtInput, exchangeInput) {
+  let currency = new Currency(amtInput, 'USD');
   CurrencyExchangeService.getCountryCodes()
     .then((countryData) => {
       // Check if repsonse is error
@@ -16,37 +15,33 @@ function getAPIData(amt, currencyName) {
         const errorMsg = `There was an error contacting the ExchangeRate API. Error: ${countryData.message}`;
         throw new Error(errorMsg);
       }
-      console.log("API Response Success!")
       let countryArray = countryData.supported_codes;
       currency.countryCodes = countryArray;
       currency = parseResults(currency);
-      parseResults(currency);
+      console.log(exchangeInput);
       printResults(currency);
-      })
-
+    })
     .catch(function(error) {
       printError(error);
-    })
-   // Validate exchange currency is supported
+    });
+  // Validate exchange currency is supported
+  return currency;
 }
 
 function parseResults(data) {
-  let dataArray = data.countryCodes;
-  let countryInput = "KWR"
-  dataArray.forEach(function(country) {
-    if (country.includes(countryInput)) {
-      data.exchangeCurrencyCode = country[0];
-      console.log(data.exchangeCurrencyCode)
-    }
-    // Add country code to exchangeRates obj
-  })
+  // dataArray.forEach(function(country) {
+  //   if (country.includes(countryInput)) {
+  //     data.exchangeCurrencyCode = country[0];
+  //     console.log(data.exchangeCurrencyCode)
+  //   }
+  // Add country code to exchangeRates obj
+  
   return data;
 }
 
 // UI Logic
 
 function printError(error) {
-  console.log(error);
   document.querySelector("p#response").innerText = error;
 }
 
@@ -58,24 +53,22 @@ function handleFormSubmission() {
   event.preventDefault();
   // Return error if user inputs nothing 
   if (!document.getElementById("input-base-currency").value || !document.getElementById("input-exchange-currency").value) {
-    document.querySelector("p#response").innerText = "Must enter base amount and currency to get exchange rate!"
-    return console.error("No input")
+    document.querySelector("p#response").innerText = "Must enter base amount and currency to get exchange rate!";
   } else {
-    console.log(getAPIData())
     // Get values of base amt & exchange currency
     const inputBaseAmt = document.getElementById("input-base-currency").value;
     const inputCurrencyName = document.getElementById("input-exchange-currency").value;
-      // Validate base amount is number
-      // Run getCurrencyCode(), store in variable
-      // const currencyCode = getCurrencyCode(inputCurrencyName)
-      console.log(inputBaseAmt, inputCurrencyName);
+    // Validate base amount is number
+    // Run getAPIData()
+    getAPIData(inputBaseAmt, inputCurrencyName);
+    // Run getCurrencyCode(), store in variable
     // Run getExchangeValue(baseAmt, currencyCode)
     // Display value in exchange currency in DOM
-    document.querySelector("p#response").innerText = "hello!"
+    document.querySelector("p#response").innerText = "hello!";
   }
 }
 
 window.addEventListener("load", function() {
   // TODO: Get country codes & store in class 
-  document.querySelector("form#exchange-form").addEventListener("submit", handleFormSubmission)
+  document.querySelector("form#exchange-form").addEventListener("submit", handleFormSubmission);
 });
